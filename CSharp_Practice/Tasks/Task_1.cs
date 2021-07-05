f085eab45e71a416e466f08dbb4ad02da7faf796
Deo Esor <RulianValeriy@yandex.ru>
2021-07-05 20:27:53 +0300

    Full 1 task( yeah-yeah)

--- CSharp_Practice/Tasks/Task_1.cs
+++ CSharp_Practice/Tasks/Task_1.cs
@@ -10,19 +10,18 @@ using System.Text;
 namespace Tasks
 {
     //TODO: ask why IEnumerable<out T> not works (or works only for interface)
-    static class Advanced_Enum<T>
-         where T : IEnumerable
+    static class Advanced_Enum
     { 
 
         /// <summary> Генерация всех возможных сочетаний из n по k
         /// 
         /// Входное перечисление: [1, 2, 3]; k == 2
-        /// 
+        ///     
         /// Выходное перечисление: [ [1, 1], [1, 2], [1, 3], [2, 2], [2, 3], [3, 3] ]
         /// 
         /// <para>int k - порядок</para>
         /// </summary>
-        public static IEnumerable<IEnumerable<T>> Combine(T[] elems,int k) => combination(elems,k);
+        public static IEnumerable<IEnumerable<T>> Combine<T>(this T[] elems,int k) => combination<T>(elems,k);
 
         /// <summary> генерация всех возможных подмножеств (без повторений)
         /// 
@@ -30,7 +29,7 @@ namespace Tasks
         /// 
         /// Выходное перечисление: [ [], [1], [2], [1, 2] ]
         /// </summary>
-        public static IEnumerable<IEnumerable<T>> Subset(T[] elems) => subsetting(elems);
+        public static IEnumerable<IEnumerable<T>> Subset<T>(this T[] elems) => subsetting<T>(elems);
 
         /// <summary> генерация всех возможных перестановок 
         /// 
@@ -38,10 +37,10 @@ namespace Tasks
         /// 
         /// Выходное перечисление: [ [1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1,2], [3, 2, 1] ]
         /// </summary>
-        public static IEnumerable<IEnumerable<T>> Permutations(T[] elems) => permutationing(elems);
+        public static IEnumerable<IEnumerable<T>> Permutations<T>(this T[] elems) => permutationing<T>(elems);
 
 
-        private static IEnumerable<IEnumerable<T>> combination(T[] elems,int k)
+        private static IEnumerable<IEnumerable<T>> combination<T>(this T[] elems,int k)
         {
             var ret = new List< IEnumerable < T >> ();
 
@@ -72,14 +71,14 @@ namespace Tasks
             return ret;
         }
 
-        private static IEnumerable<T> GetEnum(T[] mas) // попытка в корутины (в Unity просто время тыкал и было норм, а тут вот это)
+        private static IEnumerable<T> GetEnum<T>(this T[] mas) // попытка в корутины (в Unity просто время тыкал и было норм, а тут вот это)
 
         {
             foreach(T iter in mas)
                 yield return iter;
         }
 
-        private static IEnumerable<IEnumerable<T>> subsetting(T[] elems)
+        private static IEnumerable<IEnumerable<T>> subsetting<T>(this T[] elems)
         {
             var ret = new List<IEnumerable<T>>();
 
@@ -113,34 +112,48 @@ namespace Tasks
             return ret;
         }
 
-        private static IEnumerable<IEnumerable<T>> permutationing(T[] elems)
+        private static IEnumerable<IEnumerable<T>> permutationing<T>(this T[] elems)
         {
             var ret = new List<IEnumerable<T>>();
 
-            foreach(T iter in elems)
-            {
-
-                
-                
-            }
+            while (NextSet<T>(ref elems, elems.Length))
+                ret.Add(GetEnum(elems)); ;
 
             return ret;
         }
 
+        static bool NextSet<T>(ref T[] mas, int n)
+        {
+            int j = n - 2;
+            while (j != -1 ) j--;
+            if (j == -1)
+                return false; // больше перестановок нет
+            int k = n - 1;
+            while (!Advance_Comparator.Equals(mas[j],mas[k])) k--;k--;
+            swap<T>(ref mas, j, k);
+            int l = j + 1, r = n - 1; // сортируем оставшуюся часть последовательности
+            while (l < r)
+                swap<T>(ref mas, l++, r--);
+            return true;
+        }
+
+        
+
+        static void swap<T>(ref T[] mas, int i, int j)
+        {
+            T s = mas[i];
+            mas[i] = mas[j];
+            mas[j] = s;
+        }
+
     }
 
-    class Advance_Comparator<T> : IEqualityComparer<T>
+    static class Advance_Comparator
     {
-        public bool Equals([AllowNull] T x, [AllowNull] T y)
+        static bool Equals<T>([AllowNull] T x, [AllowNull] T y)
         {
             if (x.Equals(y)) return true;
             else return false;
-        }
-
-        public int GetHashCode([DisallowNull] T obj)
-        {
-            
-            throw new NotImplementedException();
         }
     }
 }
